c850377f318496847c59308e111957003d1d723d
Jie Ma <20069548@mail.wit.ie>
2015-11-08 11:44:30 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,20 @@
 class UsersController < ApplicationController
-   def show
-     @user = User.find(params[:id])
-   end
+  def show
+    @user = User.find(params[:id])
+  end
 
   def new
+  	@user = User.new
+  end
+
+  def create
+    secure_params = params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    @user = User.new(secure_params)
+    if @user.save
+       flash[:success] = "Welcome to the Twitter App!"    # NEW LINE
+       redirect_to @user
+    else
+       render 'new'     # NEW LINE    
+    end
   end
 end
